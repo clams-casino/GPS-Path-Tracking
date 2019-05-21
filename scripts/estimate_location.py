94c2d4a63ec78e1fe9dab677bb1113c69f4f6514
Mike Zhang <mike.zhang.619@gmail.com>
2019-05-21 18:40:21 -0400

    calculates coordinates in NS-EW frame based on GPS

--- scripts/estimate_location.py
+++ scripts/estimate_location.py
@@ -10,8 +10,8 @@ import numpy as np
 
 class OdomGPSEstimator:
 
-	R = 6.371e6
-	theta = 0  #amount the robot starting frame is rotated CCW from NS-EW reference frame
+	#will probably need tuning on real grizzly
+	R = 6.3781e6
 
 	def __init__(self):
 
@@ -19,40 +19,56 @@ class OdomGPSEstimator:
 		rospy.Subscriber('/navsat/fix', NavSatFix, self.navSatCallback)
 		rospy.Subscriber('/odometry/filtered', Odometry, self.odomCallback)
 
-		self.odom_x = 0
+		self.odom_x = 0  #in odom frame which maybe misaligned with the NS-EW frame
 		self.odom_y = 0
-		self.gps_x = 0
+		self.gps_x = 0   #in GPS frame which is aligned with the NS-EW frame
 		self.gps_y = 0
-		self.x = 0
+		self.x = 0       #filtered estimate for x and y in NS-EW frame
 		self.y = 0
 
-		self.yaw = 0
+		self.odom_yaw = 0
 		self.pos_cov = np.zeros((2,2))
 		self.corrected_pos_cov = np.zeros((2,2))
 		#not sure if need covariance for yaw since can't correct it anyways
 
-		
+		# need to be able to find initial yaw relative to the NS EW frame through
+		# some calibration procedure
 
+		
+		#GPS coordinates of starting location
 		self.init_long = None
 		self.init_lat = None
-		self.long = None
-		self.lat = None
+
+		#Stores data from GPS callback, spinning on a faster thread
+		self.GPS_long = None
+		self.GPS_lat = None
+
+		#Data used for calculations and estimation, at a slower rate
+		self.prev_long = None
+		self.prev_lat = None
+		self.curr_long = None
+		self.curr_lat = None
 
 
+		self.rate = rospy.Rate(50)
 
 
 	def navSatCallback(self,nav_data):
-		self.long = nav_data.longitude
-		self.lat = nav_data.latitude
+		self.GPS_long = nav_data.longitude
+		self.GPS_lat = nav_data.latitude
 
 
-
+	
 
 	def setInitNav(self):
 		while True:
-			if self.long != None:
-				self.init_long = self.long
-				self.init_lat = self.lat
+			if self.GPS_long != None:
+				self.init_long = self.GPS_long
+				self.init_lat = self.GPS_lat
+				self.prev_long = self.GPS_long
+				self.prev_lat = self.GPS_lat
+				self.curr_long = self.GPS_long
+				self.curr_lat = self.GPS_lat
 				print 'initial coordinates set as'
 				print 'longitude: ', self.init_long
 				print 'latitude: ', self.init_lat
@@ -60,13 +76,33 @@ class OdomGPSEstimator:
 
 
 	def gpsCoordinateChange(self):
-		if self.long != None:
-			dlat = self.lat - self.init_lat
-			dlong = self.long - self.init_long
+		#if getting GPS measurements
+		if self.GPS_long != None:
 
-			
+			self.curr_long = self.GPS_long
+			self.curr_lat = self.GPS_lat
 
-			#need to get xy change not just distance change
+			dlat = (self.curr_lat - self.prev_lat) * np.pi / 180
+			dlong = (self.curr_long - self.prev_long) * np.pi / 180
+
+			dgps_x = 2 * self.R * np.arcsin(np.sqrt(np.cos(self.curr_lat)**2 * np.sin(dlong/2)**2))
+			dgps_y = 2 * self.R * np.arcsin(np.sqrt(np.sin(dlat/2)**2))
+
+			if abs(self.curr_long - self.prev_long) <= 180:
+				self.gps_x += np.sign(dlong) * dgps_x #sign of the change
+
+			else:			
+				if np.sign(self.curr_long - self.prev_long) == -1:
+					self.gps_x += dgps_x
+				else:
+					self.gps_x -= dgps_x
+
+			self.gps_y += np.sign(dlat) * dgps_y 
+
+
+			self.prev_long = self.curr_long
+			self.prev_lat = self.curr_lat
+
 
 
 	def odomCallback(self,odom_data):
@@ -76,19 +112,19 @@ class OdomGPSEstimator:
 
 		quat = odom_data.pose.pose.orientation
 		euler = tf.transformations.euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
-		self.yaw = euler[2];
+		self.odom_yaw = euler[2];
 
 
 
 	def printEstimate(self):
-		print'x: ', self.x
-		print'y: ', self.y
-		print'yaw: ', self.yaw
+		
 
-		print'latitude: ', self.lat
-		print'longitude: ', self.long
+		print'longitude: ', self.curr_long
+		print'gps x: ', self.gps_x, '\n'
+		print'latitude: ', self.curr_lat
+		print'gps y: ', self.gps_y
+		print '\n\n'
 
-		print self.pos_cov
 
 
 
@@ -101,7 +137,9 @@ if __name__ == '__main__':
 		estimator.setInitNav()
 
 		while not rospy.is_shutdown():
-			print estimator.printEstimate()
+			estimator.gpsCoordinateChange()
+			estimator.printEstimate()
+			estimator.rate.sleep()
 
 	except rospy.ROSInterruptException:
 		pass
