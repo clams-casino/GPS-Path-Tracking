fixed bug with haversine, latitude units in formual now in radians

scripts/estimate_location.py
@@ -3,6 +3,7 @@
 import rospy
 from sensor_msgs.msg import NavSatFix
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 import tf
 
 import numpy as np
@@ -18,6 +19,7 @@ class OdomGPSEstimator:
 		rospy.init_node('location_estimate', anonymous=True)
 		rospy.Subscriber('/navsat/fix', NavSatFix, self.navSatCallback)
 		rospy.Subscriber('/odometry/filtered', Odometry, self.odomCallback)
+		self.vel_pub = rospy.Publisher('/grizzly_velocity_controller/cmd_vel', Twist, queue_size=5)
 
 		self.odom_x = 0  #in odom frame which maybe misaligned with the NS-EW frame
 		self.odom_y = 0
@@ -27,6 +29,8 @@ class OdomGPSEstimator:
 		self.y = 0
 
 		self.odom_yaw = 0
+		self.theta_odom_world = None
+
 		self.pos_cov = np.zeros((2,2))
 		self.corrected_pos_cov = np.zeros((2,2))
 		#not sure if need covariance for yaw since can't correct it anyways
@@ -53,7 +57,7 @@ class OdomGPSEstimator:
 		self.rate = rospy.Rate(50)
 
 
-	def navSatCallback(self,nav_data):
+	def navSatCallback(self, nav_data):
 		self.GPS_long = nav_data.longitude
 		self.GPS_lat = nav_data.latitude
 
@@ -75,6 +79,30 @@ class OdomGPSEstimator:
 				break
 
 
+
+
+	def haversineFormula(self, new_long, new_lat, old_long, old_lat):
+
+		dlat = np.deg2rad(new_lat - old_lat)
+		dlong = np.deg2rad(new_long - old_long)
+
+		dx = 2 * self.R * np.arcsin(np.sqrt(np.cos(np.deg2rad(new_lat))**2 * np.sin(dlong/2)**2))
+		dy = 2 * self.R * np.arcsin(np.sqrt(np.sin(dlat/2)**2))
+
+		if abs(dlong) <= 180:
+				dx = np.sign(dlong)*dx
+		else:			
+			if np.sign(dlong) == -1:
+				dx = abs(dx)
+			else:
+				dx = -abs(dx)
+
+		dy = np.sign(dlat)*dy
+
+		return dx, dy
+
+		
+
 	def gpsCoordinateChange(self):
 		#if getting GPS measurements
 		if self.GPS_long != None:
@@ -82,30 +110,17 @@ class OdomGPSEstimator:
 			self.curr_long = self.GPS_long
 			self.curr_lat = self.GPS_lat
 
-			dlat = (self.curr_lat - self.prev_lat) * np.pi / 180
-			dlong = (self.curr_long - self.prev_long) * np.pi / 180
+			dgps_x, dgps_y = self.haversineFormula(self.curr_long, self.curr_lat, self.prev_long, self.prev_lat)
 
-			dgps_x = 2 * self.R * np.arcsin(np.sqrt(np.cos(self.curr_lat)**2 * np.sin(dlong/2)**2))
-			dgps_y = 2 * self.R * np.arcsin(np.sqrt(np.sin(dlat/2)**2))
-
-			if abs(self.curr_long - self.prev_long) <= 180:
-				self.gps_x += np.sign(dlong) * dgps_x #sign of the change
-
-			else:			
-				if np.sign(self.curr_long - self.prev_long) == -1:
-					self.gps_x += dgps_x
-				else:
-					self.gps_x -= dgps_x
-
-			self.gps_y += np.sign(dlat) * dgps_y 
-
+			self.gps_x += dgps_x
+			self.gps_y += dgps_y
 
 			self.prev_long = self.curr_long
 			self.prev_lat = self.curr_lat
 
 
 
-	def odomCallback(self,odom_data):
+	def odomCallback(self, odom_data):
 		self.odom_x = odom_data.pose.pose.position.x
 		self.odom_y = odom_data.pose.pose.position.y
 		self.pos_cov = np.array(odom_data.pose.covariance)
@@ -113,6 +128,55 @@ class OdomGPSEstimator:
 		quat = odom_data.pose.pose.orientation
 		euler = tf.transformations.euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
 		self.odom_yaw = euler[2];
+
+
+
+	def calibrateTheta(self):
+		#finds angle that odom frame is rotated from the NS-EW (world) frame
+		#should calibrate on relatively flat ground without much in the way
+
+		#needs to use GPS callback data directly
+
+		data_rate = 1
+		calibration_speed = 0.5
+		calibration_distance = 5 #[m]
+
+		cal_rate = rospy.Rate(data_rate)
+
+		cal_data = np.zeros([2, calibration_distance/calibration_speed * data_rate])
+		cal_cmd_vel = Twist()
+
+		print 'beginning calibration data collection'
+
+		for column in xrange(cal_data.shape[1]):
+			cal_cmd_vel.linear.x = calibration_speed;
+			vel_pub.publish(cal_cmd_vel)
+			cal_rate.sleep()
+
+			cal_data[1, column] = self.GPS_long
+			cal_data[2, column] = self.GPS_lat
+
+		cal_cmd_vel.linear.x = 0
+		vel_pub.publish(cal_cmd_vel)
+		print 'finished calibration data collection, determining odom to world frame orientation'
+		print cal_data
+		sum = 0
+		for column in xrange(cal_data.shape[1]):
+			dx, dy = self.haversineFormula(cal_data[1,i], cal_data[2,i], self.init_long, self.init_lat)
+			sum += np.arctan2(dy,dx)
+
+		self.theta_odom_world = sum / cal_data.shape[1]
+		print 'odom frame is ', theta * np.pi / 180, ' degrees from the NS-EW (world) frame'
+
+		#have it drive back?
+
+
+
+		#result initialize attributes for GPS tracking
+		self.prev_long = self.GPS_long
+		self.prev_lat = self.GPS_lat
+		self.curr_long = self.GPS_long
+		self.curr_lat = self.GPS_lat
 
 
 
@@ -131,10 +195,14 @@ class OdomGPSEstimator:
 
 
 
+
+
 if __name__ == '__main__':
 	try:
 		estimator = OdomGPSEstimator()
 		estimator.setInitNav()
+
+		#estimator.calibrateTheta()
 
 		while not rospy.is_shutdown():
 			estimator.gpsCoordinateChange()